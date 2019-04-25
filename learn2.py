d12e2a094b1a45cefdc7d7030de7f7e644b7a3a7
Tshepang Maila <maddoxmaila719@gmail.com>
2019-04-25 10:02:52 +0200

    Update learn2.py

--- learn2.py
+++ learn2.py
@@ -1,8 +1,18 @@
+
+'''
+
+Author : Tshepang Maddox Maila
+Email : maddoxmaila719@gmail.com
+Post : A Simple Music Application Created Using Python Tkinter With Pygame
+
+
+'''
 import tkinter as tk
 import tkinter.filedialog as files
 import pygame
 import os
 
+#Initialize The Tkinter Module
 main = tk.Tk()
 
 class MusicApp :
@@ -13,6 +23,7 @@ class MusicApp :
 		#To hold position of the current playing song
 		self.index = 1
 		self.dir = ''
+		#Button Declarations
 		self.inputDirButton = tk.Button(self.main,text = 'Choose Directory',command=lambda:self.__ask_dir())
 		self.inputDirButton.grid(row = 0,column = 0)
 		self.PrevButton = tk.Button(self.main,text='Prev',command=lambda:self.__previous())
@@ -21,20 +32,25 @@ class MusicApp :
 		self.PlayButton.grid(row = 0, column = 2)
 		self.NextButton = tk.Button(self.main,text='Next',command=lambda:self.__next())
 		self.NextButton.grid(row = 0, column = 3)
+		#End Of Button Declaration
 
 	def __ask_dir(self) :
+		#prompts the user to choose  directory to get music from
 		self.dir = files.askdirectory()
+		#change the working directory to user provided directory
 		os.chdir(self.dir)
 		self.__choose_music()
 	def __choose_music(self) :
 		#FOR LOOP TO Choose files
 		x = 0
 		for file in os.listdir(self.dir) :
+			#only get files that of Audio Format
 			if file.endswith('.mp3') or file.endswith('.flac') :
 				self.music_files.append(os.path.realpath(file))
 	def __play(self,index) :
 		self.index = index
 		try :
+			#Using the pygame module to play music
 			pygame.mixer.init()
 			pygame.mixer.music.load(self.music_files[index])
 			pygame.mixer.music.play()
